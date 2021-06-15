9eb4ec4db30332b134a8767ffd77d12ba83316ad
GersonST <gersonsevero90@gmail.com>
2021-06-14 23:32:28 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+
+  const [nome, setNome] = useState('Gerson');
+
+  const [input, setInput] = useState('');
+
+
+
+  useEffect(()=>{
+    const tarefasStorage = localStorage.getItem('tarefas');
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+
+  }, [])
+
+
+
+  useEffect(()=>{
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+
+
+
+  const handleAdd = useCallback(()=> {
+    setTarefas([...tarefas, input])
+  }, [input, tarefas]);
+
+
+  const totalTarefas = useMemo(()=> tarefas.length, [tarefas])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
+      <br />
+
+      <strong>Você tem {totalTarefas} tarefas!</strong> <br />
+
+
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Adicionar</button> <br />
+
+     
+
+      <h1>{nome}</h1>
     </div>
   );
 }
